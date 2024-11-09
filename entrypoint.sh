b8e91acedba5f07f674ff437228d3e51ff53998d
Michael Leer <michael@gremlin.group>
2024-11-09 01:56:21 +0000

    correct var name
    Signed-off-by: Michael Leer <michael@gremlin.group>

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,12 +15,12 @@ mkdir -p /home/ssh-user/.ssh
 log "Adding files in workspace keys directory to the user's ssh authorized keys"
 
 for keyfile in /workspace/keys/*; do
-    if [ -f "$keyfile" ]; then
-        cat "$keyfile" >> /home/ssh-user/.ssh/authorized_keys
+    if [ -f "${keyfile}" ]; then
+        cat "${keyfile}" >> /home/ssh-user/.ssh/authorized_keys
         echo "" >> /home/ssh-user/.ssh/authorized_keys
         log "Authorized keys updated with ${keyfile}."
     else
-        log "Key ${key} is not a file."
+        log "Key ${keyfile} is not a file."
     fi
 done
 
