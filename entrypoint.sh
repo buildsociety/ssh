1c48e34800ba70669aad82ac4420f6a4aacb00a6
Michael Leer <michael@gremlin.group>
2024-11-09 01:50:54 +0000

    fix keys being on a single line
    Signed-off-by: Michael Leer <michael@gremlin.group>

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ log "Ensuring ssh-user has a .ssh directory"
 mkdir -p /home/ssh-user/.ssh
 
 log "Adding files in workspace keys directory to the user's ssh authorized keys"
-if find /workspace/keys -type f -exec cat {} >> /home/ssh-user/.ssh/authorized_keys \;; then
+if find /workspace/keys -type f -exec cat {} >> /home/ssh-user/.ssh/authorized_keys; echo "" >> /home/ssh-user/.ssh/authorized_keys \;; then
     log "Authorized keys updated."
 else
     log "No keys found in /workspace/keys."
