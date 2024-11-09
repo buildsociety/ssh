f560a594415dddd17d8e435f0e18e9ed6bcc5b06
Michael Leer <michael@gremlin.group>
2024-11-09 01:55:58 +0000

    more testing
    Signed-off-by: Michael Leer <michael@gremlin.group>

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,11 +13,16 @@ log "Ensuring ssh-user has a .ssh directory"
 mkdir -p /home/ssh-user/.ssh
 
 log "Adding files in workspace keys directory to the user's ssh authorized keys"
-if find /workspace/keys -type f -exec cat {} >> /home/ssh-user/.ssh/authorized_keys; echo "" >> /home/ssh-user/.ssh/authorized_keys \;; then
-    log "Authorized keys updated."
-else
-    log "No keys found in /workspace/keys."
-fi
+
+for keyfile in /workspace/keys/*; do
+    if [ -f "$keyfile" ]; then
+        cat "$keyfile" >> /home/ssh-user/.ssh/authorized_keys
+        echo "" >> /home/ssh-user/.ssh/authorized_keys
+        log "Authorized keys updated with ${keyfile}."
+    else
+        log "Key ${key} is not a file."
+    fi
+done
 
 log "Starting the SSH server in the foreground"
 /usr/sbin/sshd -D -e
